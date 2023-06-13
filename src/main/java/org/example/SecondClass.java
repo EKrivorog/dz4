99b327cb9a43bb3ba3fb745bb81f1637b6c4487d
Evgeniy Krivorog <Evgeniy.krivorog@gmail.com>
2023-06-13 21:06:56 +0300

    Створено 2 класи

--- src/main/java/org/example/SecondClass.java
+++ src/main/java/org/example/SecondClass.java
@@ -1,2 +1,9 @@
-package org.example;public class SecondClass {
+package org.example;
+
+public class SecondClass {
+    int fieldValue = 777;
+    public int printAndReturnField() {
+        System.out.println("---" + fieldValue + "---");
+        return fieldValue;
+    }
 }
